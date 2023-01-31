94eee52500d32b2d836561f421d08446629daedc
huayra2115 <109313548+huayra2115@users.noreply.github.com>
2023-01-31 21:03:51 +0100

    komenda admin/ zasady serwera/ opgg profil

--- Commands/komendyLewandowski.cs
+++ Commands/komendyLewandowski.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -6,9 +7,56 @@ using DSharpPlus.Entities;
 
 public class komendyLewandowski : BaseCommandModule
 {
-    [Command("ping2")]
-    public async Task PongCommand(CommandContext ctx)
+    [Command("zasady")]
+    public async Task ZasadyCommand(CommandContext ctx)
     {
-        await ctx.RespondAsync($"Pong!");
+
+        var ZasadyEmbed = new DiscordEmbedBuilder
+        {
+            Title = "Zasady Serwera",
+            Color = DiscordColor.Yellow,
+            Description = "Zasady" + System.Environment.NewLine + "1. Zakaz umieszczania treści obrażających inne rasy i mniejszości narodowe" + System.Environment.NewLine + "2. Zakaz spamowania" + System.Environment.NewLine + "3. Zakaz wysyłania treści NSFW" + System.Environment.NewLine + "4. Zakaz oznaczania bez powodu" + System.Environment.NewLine + "5. Zakaz posiadania i tworzenia multikont na serwerze" + System.Environment.NewLine + "6. Zakaz leakowania danych osobowych i prywatnych informacji takich jak: imię, nazwisko, adres, numer telefonu itp" + System.Environment.NewLine + "7. Admin ma zawsze rację"
+
+        };
+
+        var ZasadyMessage = await ctx.Channel.SendMessageAsync(embed: ZasadyEmbed).ConfigureAwait(false);
+
+
+
     }
+
+    [Command("opgg")]
+    public async Task OpggCommand(CommandContext ctx)
+    {
+        var OpggEmbed = new DiscordEmbedBuilder
+
+        {
+            Title = "Link do strony Opgg z profilem gracza",
+            Color = DiscordColor.Blue,
+            Description = "Link do opgg oraz do najlepszych obecnie postaci z gry pod względem wygranych " + System.Environment.NewLine + "https://www.op.gg" + System.Environment.NewLine + "https://www.op.gg/champions"
+        };
+
+        var opggMessage = await ctx.Channel.SendMessageAsync(embed: OpggEmbed).ConfigureAwait(false);
+
+    }
+    [Command("Admin")]
+    public async Task adminCommand(CommandContext ctx)
+    {
+        var adminEmbed = new DiscordEmbedBuilder
+        {
+            Title = "Admin/Admini serwera",
+            Color = DiscordColor.Purple,
+            Description = "Admini Serwera" + System.Environment.NewLine + "Gabriel Mróz" + System.Environment.NewLine + "Jakub Lewandowski"
+
+        };
+
+        var AdminMessage = await ctx.Channel.SendMessageAsync(embed: adminEmbed).ConfigureAwait(false);
+
+    }
+    
 }
+
+
+
+
+        
